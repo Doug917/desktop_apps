6ce419772e54feaf4f5cfb4c79f6fc2f3170bb72
Doug <123341895+Doug917@users.noreply.github.com>
2024-02-08 22:05:25 -0700

    Update concordance.py

--- Concordance/concordance.py
+++ Concordance/concordance.py
@@ -43,12 +43,3 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-
-
-
-
-#Handle user input.
-
-limit = int(input("Limit number of search results to: "))
-count = 0
-query = input("Enter phrase: ")
